2566a7eeeef6cf1588b80783925215ae311f785b
Slippermaks <Slippermaks@yandex.ru>
2021-10-07 23:58:55 +0300

    Added the ability to send messages. Added filters for messages.

---
+++ src/main/java/com/example/MEssg/repos/MessageRepo.java
@@ -0,0 +1,11 @@
+package com.example.MEssg.repos;
+
+import com.example.MEssg.domain.Message;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface MessageRepo extends CrudRepository<Message, Long> {
+
+    List<Message> findByTag(String tag);
+}
