0ee8403eaf8a4524da61b9b552dae8592a1a217e
Slippermaks <Slippermaks@yandex.ru>
2021-10-14 22:14:44 +0300

    Added "Email" field for registration. Added email activation code notification.

--- src/main/java/com/example/MEssg/service/UserService.java
+++ src/main/java/com/example/MEssg/service/UserService.java
@@ -1,16 +1,25 @@
 package com.example.MEssg.service;
 
+import com.example.MEssg.domain.Role;
+import com.example.MEssg.domain.User;
 import com.example.MEssg.repos.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+import java.util.Collections;
+import java.util.UUID;
 
 @Service
 public class UserService implements UserDetailsService {
     //@Autowired
     private final UserRepo userRepo;
+
+    @Autowired
+    private MailSender mailSender;
 
     public UserService(UserRepo userRepo) {
         this.userRepo = userRepo;
@@ -19,5 +28,45 @@ public class UserService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return userRepo.findByUsername(username);
+    }
+
+    public boolean addUser(User user) {
+        User userFromDb = userRepo.findByUsername(user.getUsername());
+
+        if (userFromDb != null) {
+            return false;
+        }
+
+        user.setActive(true);
+        user.setRoles(Collections.singleton(Role.USER));
+        user.setActivationCode(UUID.randomUUID().toString());
+        userRepo.save(user);
+
+        if (!StringUtils.isEmpty(user.getEmail())) {
+            String message = String.format(
+                    "Hello, %s! \n" +
+                            "Welcome to MEssg. Please, visit next link: http://localhost:8080/activate/%s",
+                    user.getUsername(),
+                    user.getActivationCode()
+            );
+
+            mailSender.send(user.getEmail(), "Activation code", message);
+        }
+
+        return true;
+    }
+
+    public boolean activateUser(String code) {
+        User user = userRepo.findByActivationCode(code);
+
+        if (user == null) {
+            return false;
+        }
+
+        user.setActivationCode(null);
+
+        userRepo.save(user);
+
+        return true;
     }
 }
--- src/main/resources/templates/parts/login.ftl
+++ src/main/resources/templates/parts/login.ftl
@@ -1,18 +1,28 @@
 <#macro login path isRegisterForm>
     <form action="${path}" method="post">
+        <#if isRegisterForm>
+            <div class="form-group row">
+                <label class="col-sm-2 col-form-label"> Email:</label>
+                <div class="col-sm-5">
+                    <input class="form-control" type="email" name="email" placeholder="Example@ex.com"/>
+                </div>
+            </div>
+        </#if>
+
         <div class="form-group row">
             <label class="col-sm-2 col-form-label"> User Name :</label>
             <div class="col-sm-5">
                 <input class="form-control" type="text" name="username" placeholder="Username"/>
             </div>
-
         </div>
+
         <div class="form-group row">
             <label class="col-sm-2 col-form-label"> Password:</label>
             <div class="col-sm-5">
                 <input class="form-control" type="password" name="password" placeholder="Password"/>
             </div>
         </div>
+
         <input type="hidden" name="_csrf" value="${_csrf.token}"/>
         <#if !isRegisterForm><a href="/registration">Registration</a></#if>
         <button type="submit" class="btn btn-primary"><#if isRegisterForm>Create<#else>Sign In</#if></button>
