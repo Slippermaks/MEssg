f2ca052d546c83957a9fed1814e33202911cd2b9
Slippermaks <Slippermaks@yandex.ru>
2021-10-18 23:43:03 +0300

    Added been validation, password encryption. Added reCaptcha, rememberMe and saving sessions in the DB.

--- src/main/java/com/example/MEssg/service/UserService.java
+++ src/main/java/com/example/MEssg/service/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
@@ -15,19 +16,24 @@ import java.util.stream.Collectors;
 
 @Service
 public class UserService implements UserDetailsService {
-    //@Autowired
-    private final UserRepo userRepo;
+    @Autowired
+    private UserRepo userRepo;
 
     @Autowired
     private MailSender mailSender;
 
-    public UserService(UserRepo userRepo) {
-        this.userRepo = userRepo;
-    }
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return userRepo.findByUsername(username);
+        User user = userRepo.findByUsername(username);
+
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found");
+        }
+
+        return user;
     }
 
     public boolean addUser(User user) {
@@ -40,6 +46,8 @@ public class UserService implements UserDetailsService {
         user.setActive(true);
         user.setRoles(Collections.singleton(Role.USER));
         user.setActivationCode(UUID.randomUUID().toString());
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+
         userRepo.save(user);
 
         sendMessage(user);
